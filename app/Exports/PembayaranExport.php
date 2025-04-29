f1693fcd91b2d4e3740b4de145720a641358992d
Sahara <164311327+saharakhaerunnisa@users.noreply.github.com>
2025-04-29 09:13:39 +0700

    push

--- app/Exports/PembayaranExport.php
+++ app/Exports/PembayaranExport.php
@@ -3,9 +3,11 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
 use App\Models\Pembayaran;
 
-class PembayaranExport implements FromView
+class PembayaranExport implements FromView, WithHeadings, WithTitle
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -17,4 +19,9 @@ class PembayaranExport implements FromView
             'isExport' => true,
         ]);
     }
+
+    // public function headings(): array
+    // {
+    //     return ['ID Pembayaran', 'Tagihan ID', 'Tanggal Pembayaran', 'Jumlah Pembayaran', 'Keterangan'];
+    // }
 }
